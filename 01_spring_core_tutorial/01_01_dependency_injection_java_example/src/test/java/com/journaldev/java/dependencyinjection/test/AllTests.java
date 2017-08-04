71f09ff54bd7659fe9a0edadabf22b66a3750794
Rudolf Rada <rudolf@rudolf-treti.cz>
2017-08-05 00:04:04 +0200

    repair junit tests

--- 01_spring_core_tutorial/01_01_dependency_injection_java_example/src/test/java/com/journaldev/java/dependencyinjection/test/AllTests.java
+++ 01_spring_core_tutorial/01_01_dependency_injection_java_example/src/test/java/com/journaldev/java/dependencyinjection/test/AllTests.java
@@ -7,6 +7,6 @@ import org.junit.runners.Suite;
  * Created by radar on 23.5.15.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({JUnitTest.class})
+@Suite.SuiteClasses({ MyDIApplicationJUnitTest.class})
 public class AllTests {
 }
--- 01_spring_core_tutorial/01_01_dependency_injection_java_example/src/test/java/com/journaldev/java/dependencyinjection/test/MyDIApplicationJUnitTest.java
+++ 01_spring_core_tutorial/01_01_dependency_injection_java_example/src/test/java/com/journaldev/java/dependencyinjection/test/MyDIApplicationJUnitTest.java
@@ -1,46 +1,59 @@
 package com.journaldev.java.dependencyinjection.test;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import com.journaldev.java.dependencyinjection.consumer.Consumer;
 import com.journaldev.java.dependencyinjection.consumer.MyDIApplication;
 import com.journaldev.java.dependencyinjection.injector.MessageServiceInjector;
 import com.journaldev.java.dependencyinjection.service.MessageService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyDIApplicationJUnitTest {
 
+	private Logger logger = LoggerFactory.getLogger(MyDIApplicationJUnitTest.class);
+
 	private MessageServiceInjector injector;
+
 	@Before
 	public void setUp(){
 		//mock the injector with anonymous class
 		injector = new MessageServiceInjector() {
 			
-			@Override
+
 			public Consumer getConsumer() {
 				//mock the message service
 				return new MyDIApplication(new MessageService() {
-					
-					@Override
+
 					public void sendMessage(String msg, String rec) {
-						System.out.println("Mock Message Service implementation");
-						
+						logger.debug("Mock Message Service implementation");
 					}
+
 				});
 			}
 		};
+		logger.debug("injector has been created");
 	}
-	
+
 	@Test
-	public void test() {
+	public void test0()  {
+		Assert.assertNotNull(injector);
+	}
+
+	@Test
+	public void test1() {
 		Consumer consumer = injector.getConsumer();
 		consumer.processMessages("Hi Pankaj", "pankaj@abc.com");
+		logger.debug("Message has been sent");
 	}
 	
 	@After
 	public void tear(){
 		injector = null;
+		Assert.assertNull(injector);
+		logger.debug("injector is set to null");
 	}
 
 }
